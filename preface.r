32123e52f62866381953c1c74d1e07e0901849a2
wjmanzon <wilsonjr.manzon@gmail.com>
2024-03-23 20:04:54 -0600

    preface

---
+++ preface.r
@@ -0,0 +1,15 @@
+
+# install tidyverse
+install.packages("tidyverse")
+
+# you will not be able to use functions, objects, and help files
+# in a package until you load it with library()
+library(tidyverse)
+
+# check if your version of tidyverse is updated
+tidyverse_update()
+
+# these packages provide data on airline flights,
+# world development, and baseball
+install.packages(c("nycflights13","gapminder","Lahman"))
+
